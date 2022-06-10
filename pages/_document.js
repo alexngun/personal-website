d380da1e8e2d4a3abe357e176ed20375d51e4458
Alex Ngun <alexngun7@gmail.com>
2022-06-10 07:40:41 -0600

    update icon

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
         <Head>
             <meta name="description" content="Alex's homepage" />
             <meta name="author" content="Alex Ngun" />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/icons/boy.png" />
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
             <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,300;0,400;0,500;0,700;1,400&family=Monoton&family=Pacifico&display=swap" rel="stylesheet"/>
